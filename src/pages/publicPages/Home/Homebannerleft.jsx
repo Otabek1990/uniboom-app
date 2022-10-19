6379ebcc5b2bfcfe6ce031ddd431de4aff79478d
Baxtiyor <bakhtiyor101@gmail.com>
2022-10-19 14:09:11 +0500

    feat(uniboom-3-homepage-banner)-banner-added

--- src/pages/publicPages/Home/Homebannerleft.jsx
+++ src/pages/publicPages/Home/Homebannerleft.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
-
-import phonebg from '../../assets/images/slider_1_1.png'
+import phonebg from '../../../assets/images/slider_1_1.png'
 
 function Homebannerleft() {
   return (
-    <div className=" container col-span-3  ">
+    <div className=" container col-span-3">
       <h5 className="text-md text-black font-semibold">
         Будьте в курсе происходящего
       </h5>
-      <div className=" rounded-xl gap-16 max-w-full flex justify-between items-center px-12  bg-[url('/src/assets/images/slider_1.png')] bg-no-repeat bg-center bg-cover mt-8">
+      <div className=" rounded-xl gap-16 h-[500px] max-w-full flex justify-between items-center   p-24 pb-12   bg-[url('/src/assets/images/slider_1.png')] bg-no-repeat bg-center bg-cover mt-8">
         <div className="">
-          <h2 className=" mb-4 font-semibold text-lg">
-            Тяжёлое машиностроение — это <br /> вам не песнь светлого будущего
+          <h2 className=" mb-4 font-bold text-lg">
+            Тяжёлое машиностроение — это вам не песнь светлого будущего
           </h2>
           <span className="text-base font-normal text-black opacity-50  ">
-            {' '}
             Вот вам яркий пример современных тенденций — <br /> экономическая
             повестка сегодняшнего дня создаёт.
           </span>
@@ -24,9 +22,8 @@ function Homebannerleft() {
           <i className="fa fa-angle-left  mr-2 bg-gray-200 bg-opacity-30 py-3 px-4 rounded hover:shadow-xl"></i>
           <i className="fa fa-angle-right bg-gray-200 bg-opacity-30  py-3 px-4 rounded hover:shadow-xl"></i>
         </div>
-        <div className="  ">
-          <img src={phonebg} alt="" />
-        </div>
+
+        <img className=" h-[500px]" src={phonebg} alt="" />
       </div>
     </div>
   )
--- src/pages/publicPages/Home/Homebannerright.jsx
+++ src/pages/publicPages/Home/Homebannerright.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
-
-import compbg from '../../assets/images/product_1.png'
+import compbg from '../../../assets/images/product_1.png'
 
 function Homebannerright() {
   return (
     <div className=" col-span-1  ">
       <h5 className="text-md text-black font-semibold mb-8">Товары дня</h5>
-      <div className="  pt-14 pb-20  px-16 border-0 bg-white-300 r rounded-lg  inline-block text-center  hover:shadow-lg  ">
+      <div className=" h-[500px] pt-14 pb-20  px-16 border-0 bg-white-300 r rounded-lg  inline-block text-center  hover:shadow-lg  ">
         <img src={compbg} alt="" />
         <p className=" font-normal text-thin  ">
-          {' '}
-          MacBook Pro 13 inch 8/256/A2338{' '}
+          MacBook Pro 13 inch 8/256/A2338
         </p>
         <span className=" font-md text-black mt-2.5 font-semibold block ">
           14 000 000 сум
