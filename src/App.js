dfda30b0f82b2751d7d7f0b28c22cd385b5192bd
saman <samanmulla@icloud.com>
2022-10-26 18:12:55 +0500

    Profile page is redy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import Profile from './pages/privatePages/Profile'
 import SignedUser from './SignedUser.jsx'
 import UnsignedUser from './UnsignedUser.jsx'
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="">
       <Header />
+      <Profile />
       {loggedIn ? <SignedUser /> : <UnsignedUser />}
 
       <Footer />
